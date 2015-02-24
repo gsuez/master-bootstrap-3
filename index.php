adaa8ad9f9df18dcb13984780057a9ff2755bef9
Gonzalo Suez <info@gsuez.cl>
2015-02-24 18:19:38 -0300

    update for new params

--- index.php
+++ index.php
@@ -1,39 +1,34 @@
 <?php
-        /*------------------------------------------------------------------------
+	/*------------------------------------------------------------------------
 # author Gonzalo Suez
-# copyright © 2013 gsuez.cl. All rights reserved.
+# copyright Copyright © 2013 gsuez.cl. All rights reserved.
 # @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
 # Website http://www.gsuez.cl
--------------------------------------------------------------------------*/
-// no direct access
-defined('_JEXEC') or die;
-
-include 'includes/params.php';
-?>
-<!doctype html>
-<html lang="en" >
-
+-------------------------------------------------------------------------*/	// no direct access
+	defined('_JEXEC') or die;
+	include 'includes/params.php';
+	?>
+<!DOCTYPE html>
+<html lang="en">
 <?php
-include 'includes/head.php';
-?>
-
+ include 'includes/head.php'; ?>
 <body>
 <!--top-->
-<?php if($this->countModules('top')) : ?>
-<div id="top">
+<?php  if($this->countModules('top')) : ?>
+<div id="top" class="navbar-inverse">
 <div class="container">
 <div class="row">
 <jdoc:include type="modules" name="top" style="none" />        
 </div>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!--top-->
 <div id="wrap">
 <!--Navigation-->
 <div id="navigation">
-<div class="container">
 <div class="navbar navbar-default" role="navigation">
+<div class="container">
 <div class="navbar-header">
 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
 <span class="sr-only">Toggle navigation</span>
@@ -42,33 +37,33 @@ include 'includes/head.php';
 <span class="icon-bar"></span>
 </button>
                 <div id="brand">
-                                <a href="<?php echo $this->params->get('logo_link') ?>">
-                                         <img style="width:<?php echo $this->params->get('logo_width') ?>px; height:<?php echo $this->params->get('logo_height') ?>px; " src="<?php echo $this->params->get('logo_file') ?>" alt="Logo" />
+                                <a href="<?php  echo $this->params->get('logo_link')   ?>">
+                                         <img style="width:<?php  echo $this->params->get('logo_width')   ?>px; height:<?php  echo $this->params->get('logo_height')   ?>px; " src="<?php  echo $this->params->get('logo_file')   ?>" alt="Logo" />
                                         </a>
                         </div>
 </div>
 <div class="navbar-collapse collapse">
-<?php if ($this->countModules('navigation')) : ?>
+<?php  if ($this->countModules('navigation')) : ?>
                         <nav class="navigation" role="navigation">
                                 <jdoc:include type="modules" name="navigation" style="none" />
                         </nav>
-                        <?php endif; ?>
+                        <?php  endif; ?>
 </div>
 </div>
 </div>
 </div>
 <!--Navigation-->
 <!--fullwidth-->
-<?php if($this->countModules('fullwidth')) : ?>
+<?php  if($this->countModules('fullwidth')) : ?>
 <div id="fullwidth">
 <div class="row">
 <jdoc:include type="modules" name="fullwidth" style="block"/>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!--fullwidth-->
 <!--Showcase-->
-<?php if($this->countModules('showcase')) : ?>
+<?php  if($this->countModules('showcase')) : ?>
 <div id="showcase">
 <div class="container">
 <div class="row">
@@ -76,10 +71,10 @@ include 'includes/head.php';
 </div>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!--Showcase-->
 <!--Feature-->
-<?php if($this->countModules('feature')) : ?>
+<?php  if($this->countModules('feature')) : ?>
 <div id="feature">
 <div class="container">
 <div class="row">
@@ -87,81 +82,77 @@ include 'includes/head.php';
 </div>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!--Feature-->
 <!-- Content -->
 <div class="container">
-<?php if($this->countModules('breadcrumbs')) : ?>
+<?php  if($this->countModules('breadcrumbs')) : ?>
 <div id="breadcrumbs">        
-
 <div class="row">
 <jdoc:include type="modules" name="breadcrumbs" style="block" />
 </div>
-
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <div id="main" class="row show-grid">
 <!-- Left -->
-<?php if($this->countModules('left')) : ?>
-<div id="sidebar" class="col-sm-<?php echo $leftcolgrid; ?>">
-<jdoc:include type="modules" name="left" style="xhtml" />
+<?php  if($this->countModules('left')) : ?>
+<div id="sidebar" class="col-sm-<?php  echo $leftcolgrid; ?>">
+<jdoc:include type="modules" name="left" style="block" />
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!-- Component -->
-<div id="container" class="col-sm-<?php echo (12-$leftcolgrid-$rightcolgrid); ?>">
+<div id="container" class="col-sm-<?php  echo (12-$leftcolgrid-$rightcolgrid); ?>">
 <!-- Content-top Module Position -->        
-<?php if($this->countModules('content-top')) : ?>
+<?php  if($this->countModules('content-top')) : ?>
 <div id="content-top">
 <div class="row">
 <jdoc:include type="modules" name="content-top" style="block" />        
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!-- Front page show or hide -->    
 <?php
-$app = JFactory::getApplication();
-$menu = $app->getMenu();
-						
-if ($frontpageshow){ 
-// show on all pages
-?>
+	$app = JFactory::getApplication();
+	$menu = $app->getMenu();
+	
+	if ($frontpageshow){
+		// show on all pages
+		?>
 <div id="main-box">
 <jdoc:include type="component" />
 </div>
 <?php 
-}
-else {
-if ($menu->getActive() !== $menu->getDefault()) {
-// show on all pages but the default page
-?>
+	} else {
+		
+		if ($menu->getActive() !== $menu->getDefault()) {
+			// show on all pages but the default page
+			?>
 <div id="main-box">
 <jdoc:include type="component" />
 </div>
 <?php
-} 
-}
-?>	
+ }} ?>	
 <!-- Front page show or hide -->    		
 <!-- Below Content Module Position -->        
-<?php if($this->countModules('content-bottom')) : ?>
+<?php  if($this->countModules('content-bottom')) : ?>
 <div id="content-bottom">
 <div class="row">
 <jdoc:include type="modules" name="content-bottom" style="block" />	
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 </div>
 <!-- Right -->
-<?php if($this->countModules('right')) : ?>
-<div id="sidebar-2" class="col-sm-<?php echo $rightcolgrid; ?>">
-<jdoc:include type="modules" name="right" style="xhtml" />
+<?php  if($this->countModules('right')) : ?>
+<div id="sidebar-2" class="col-sm-<?php  echo $rightcolgrid; ?>">
+<jdoc:include type="modules" name="right" style="block" />
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 </div>
 </div>
 <!-- Content -->
 <!-- bottom -->
-<?php if($this->countModules('bottom')) : ?>
+<?php  if($this->countModules('bottom')) : ?>
 <div id="bottom">
 <div class="container">
 <div class="row">
@@ -169,26 +160,25 @@ if ($menu->getActive() !== $menu->getDefault()) {
 </div>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <div id="push"></div>
 <!-- bottom -->
 </div>
 <!-- footer -->
-<?php if($this->countModules('footer')) : ?>
-<div id="footer">
+<?php  if($this->countModules('footer')) : ?>
+<div id="footer" class="well">
 <div class="container">
 <div class="row">
 <jdoc:include type="modules" name="footer" style="block" />
 </div>
 </div>
 </div>
-<?php endif; ?>
+<?php  endif; ?>
 <!-- footer -->
-
+<a href="#" class="back-to-top">Back to Top</a>
 <jdoc:include type="modules" name="debug" />        
 <!-- page -->        
 <!-- JS -->
-<script src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/bootstrap.min.js"></script>
 <script type="text/javascript">
 (function($){
 $(document).ready(function(){
@@ -203,18 +193,55 @@ $('.parent > ul').addClass('dropdown-menu');
 });
 })(jQuery);
 </script>
+<!-- Fix hide dropdown -->
 <script type="text/javascript">
-$('.dropdown input').click(function(e) {
-e.stopPropagation();
-});
+if (MooTools != undefined) {
+    var mHide = Element.prototype.hide;
+    Element.implement({
+        hide: function() {
+                if (this.hasClass("deeper")) {
+                    return this;
+                }
+                mHide.apply(this, arguments);
+            }
+    });
+}
+</script>
+<!-- Fix hide dropdown -->
+<script type="text/javascript">
+jQuery(function($){
+    $('.dropdown-menu input, .dropdown-menu label').click(function(e) {
+        e.stopPropagation();
+    });
+	});
 </script>
 <script type="text/javascript">
-$('.dropdown-menu .dropdown-submenu a[data-toggle="dropdown-submenu"]').click(function (e)
-{                   
-    e.stopPropagation();
+		jQuery( document ).ready(function() {
+jQuery('.tooltip').tooltip({
+    html: true
 });
-</script>
+});
+    </script>
 
+<!-- To Top -->
+        <script>            
+			jQuery(document).ready(function() {
+				var offset = 220;
+				var duration = 500;
+				jQuery(window).scroll(function() {
+					if (jQuery(this).scrollTop() > offset) {
+						jQuery('.back-to-top').fadeIn(duration);
+					} else {
+						jQuery('.back-to-top').fadeOut(duration);
+					}
+				});
+				jQuery('.back-to-top').click(function(event) {
+					event.preventDefault();
+					jQuery('html, body').animate({scrollTop: 0}, duration);
+					return false;
+				})
+			});
+		</script>
 <!-- JS -->
 </body>
 </html>
