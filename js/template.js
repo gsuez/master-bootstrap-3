bbdf476d2d5b50e569e27b8355d28f04767da54d
Gonzalo Suez <info@gsuez.cl>
2015-10-23 16:29:22 -0300

    New headroom js, upgrade bootstrap

--- js/template.js
+++ js/template.js
@@ -20,6 +20,12 @@ if (typeof MooTools != 'undefined') {
 			$('.parent > a').append('<b class="caret"></b>');
 			$('.parent > ul').addClass('dropdown-menu');
 		}
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
 		// Fix hide dropdown
 		$('.dropdown-menu input, .dropdown-menu label').click(function(e) {
 			e.stopPropagation();
