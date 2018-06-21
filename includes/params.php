1ced9c502cbac8a90af1aa1838519afbc2498060
daloonik <daloonik@gmail.com>
2018-06-21 10:32:23 -0400

    Added FB Open Graph and Twitter Card and customizable css option

--- includes/params.php
+++ includes/params.php
@@ -23,6 +23,8 @@
 	$doc->addStyleSheet('templates/' . $this->template . '/css/bootstrap.min.css');
 	$doc->addStyleSheet('templates/' . $this->template . '/css/icons.css');
 	$doc->addStyleSheet('templates/' . $this->template . '/css/template.min.css');
+    //Add Custom Stylesheet
+    $doc->addStyleSheet('templates/' . $this->template . '/css/user.css');
 	// Variables
 	$headdata = $doc->getHeadData();
 	$menu = $app->getMenu();
